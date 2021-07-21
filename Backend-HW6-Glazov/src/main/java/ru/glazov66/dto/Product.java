6fcb76c68f348df3edb991cc9090cf869596664f
E <g>
2021-07-22 02:28:18 +0300

    HW-6_Glazov

---
+++ Backend-HW6-Glazov/src/main/java/ru/glazov66/dto/Product.java
@@ -0,0 +1,16 @@
+package ru.glazov66.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+@ToString
+public class Product {
+    Integer id;
+    String title;
+    Integer price;
+    String categoryTitle;
+
+}
